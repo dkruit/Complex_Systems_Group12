c8c081292f210f60ae2310479bc5bdb0aca754d5
Zaida Rivai <zaida.rivai@gmail.com>
2020-06-17 14:05:46 +0200

    WIP fast dynamics

--- fast_dynamics.py
+++ fast_dynamics.py
@@ -15,17 +15,19 @@ gen_layer = 3
 
 A, P_0 = initialize_model(n_buses, max_P_gen, layers, gen_layer, max_F)
 Fk = np.dot(A,P_0)
-# Fjk = np.zeros([Fk])
-# Mjk = []
 
-# print(A.shape)
-# #Define M
-# for i in range(len(F)):
-Mjk = [i/max_F for i in Fk]
-P_outaged = [3*i + 5 for i in Mjk]
-P_outaged_sum = sum(P_outaged)
-P_outaged = [i/P_outaged_sum for i in P_outaged] #give probabilities
-print(P_outaged)
-# P_overload= [3*i + 5 for i in Mjk]
+# #Define M for day k
+#TODO: loop over days to get M
+days = 10
+for k in days:
+    A, P_0 = initialize_model(n_buses, max_P_gen, layers, gen_layer, max_F)
+    Fk = np.dot(A,P_0)
+    Mk = [i/max_F for i in Fk]
+    P_outaged = [0.001*i for i in Mjk]
+    P_outaged_sum = sum(P_outaged)
+    P_outaged = [i/P_outaged_sum for i in P_outaged] #give probabilities
+    P_overload = [0.3 *i for i in Mjk]
+    P_overload_sum = sum(P_outaged)
+    P_overload = [i/P_overload_sum for i in P_overload] #give probabilities
 
-# print(P_outaged)
+    print("P_outaged is {},\n P_overload is {},\n".format(P_outaged,P_overload))
